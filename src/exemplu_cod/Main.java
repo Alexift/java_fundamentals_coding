f30067e69dc577cbb003826a7017488cc4544357
Alex Iftime <alecs.iftime@gmail.com>
2024-08-01 20:40:35 +0300

    Am creat clasa Dog si clasa Main

--- src/exemplu_cod/Main.java
+++ src/exemplu_cod/Main.java
@@ -1,58 +1,58 @@
-package com.sda;
-
-// {} => semnifica un bloc de cod / set de instructiuni / delimiteaza codul scris de noi
-
-// In java putem scrie cod DOAR IN INTERIORUL ACOLADELOR
-
-// Clasa => Bloc de apartamente
-// Metoda => etajul din bloc
-// if => un apartament
-// for => o camera din apartament
-
-public class BlocApartamente { // bloc de apartamente
-    // variabila globala
-    String administrator = "";
-
-    public static void etajul1() { // etajul 1
-        //variabile locale in metoda
-        String ghiveciFlori = "Petunie";
-
-        boolean isTrue = true;
-
-        if(isTrue == true) { // apartament
-            String aparatClima = "Boch";
-
-            int lines = 10;
-
-            System.out.println(isTrue);
-            System.out.println(lines);
-
-            for(int i = 0; i < 10; i++) { // camera
-                // i este vizibil doar in una camere
-                System.out.println(i);
-
-                String laptop = "MacBook";
-
-                System.out.println(aparatClima);
-
-                System.out.println(isTrue);
-            }
-        }
-
-        if(1 > 0) {
-            System.out.println("da unu e mai mare ca 0");
-        }
-
-        System.out.println("Este in if?");
-
-//        System.out.println(aparatClima);
-
-        System.out.println(isTrue);
-    }
-
-    public static void etajul2(String message) { // etajul 2
-
-    }
-
-
-}
+//package com.sda;
+//
+//// {} => semnifica un bloc de cod / set de instructiuni / delimiteaza codul scris de noi
+//
+//// In java putem scrie cod DOAR IN INTERIORUL ACOLADELOR
+//
+//// Clasa => Bloc de apartamente
+//// Metoda => etajul din bloc
+//// if => un apartament
+//// for => o camera din apartament
+//
+//public class BlocApartamente { // bloc de apartamente
+//    // variabila globala
+//    String administrator = "";
+//
+//    public static void etajul1() { // etajul 1
+//        //variabile locale in metoda
+//        String ghiveciFlori = "Petunie";
+//
+//        boolean isTrue = true;
+//
+//        if(isTrue == true) { // apartament
+//            String aparatClima = "Boch";
+//
+//            int lines = 10;
+//
+//            System.out.println(isTrue);
+//            System.out.println(lines);
+//
+//            for(int i = 0; i < 10; i++) { // camera
+//                // i este vizibil doar in una camere
+//                System.out.println(i);
+//
+//                String laptop = "MacBook";
+//
+//                System.out.println(aparatClima);
+//
+//                System.out.println(isTrue);
+//            }
+//        }
+//
+//        if(1 > 0) {
+//            System.out.println("da unu e mai mare ca 0");
+//        }
+//
+//        System.out.println("Este in if?");
+//
+////        System.out.println(aparatClima);
+//
+//        System.out.println(isTrue);
+//    }
+//
+//    public static void etajul2(String message) { // etajul 2
+//
+//    }
+//
+//
+//}
--- src/shopping_app/ShoppingManager.java
+++ src/shopping_app/ShoppingManager.java
@@ -2,9 +2,9 @@ package shopping_app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ShoppingManager {
-
 
 
     //Propietati :
@@ -17,7 +17,67 @@ public class ShoppingManager {
     public void start() {
         System.out.println("Shopping List App");
 
-        showMenu();
+        Scanner scanner = new Scanner(System.in);
+
+        Boolean isRunnung = true;
+
+        while (isRunnung) {
+
+            showMenu();
+
+            String option = scanner.next();
+            scanner.nextLine();
+
+
+            switch (option) {
+
+                case "1":
+                    System.out.println("introduceti un Item :");
+                    String item = scanner.nextLine();
+                    addItem(item);
+                    break;
+
+                case "2":
+                    System.out.println("Introduceti Item-ul pe care doriti sa il modificati:");
+                    String oldItem = scanner.nextLine();
+
+                    System.out.println("Introduceti item-ul now");
+                    String newItem = scanner.nextLine();
+
+                    updateItem(oldItem, newItem);
+                    break;
+
+                case "3":
+                    System.out.println("introduceti un Item pentru stergere :");
+
+                    String itemForDelete = scanner.nextLine();
+                    deleteItem(itemForDelete);
+                    break;
+
+                case "4":
+                    showItemsList();
+                    break;
+
+                case "5":
+                    itemsList.clear();
+                    break;
+
+                case "6":
+                    isRunnung = false;
+                    break;
+
+                default:
+                    System.out.println("Optiunea nu exista !");
+
+
+            }
+            System.out.println("---------------------------------------");
+
+        }
+        // variabila item de mai sus stocheaza cea ce citim de la tastatura
+        // metoda addItem() , primeste ca si parametru o variabila de tip String , aceasta variabila contine valoare citita de la tastat.
+        // addItem(item) - prin aceasta linie trmitem valoare citita din moetoda start() , in metoda addItem.
+
 
     }
 
@@ -41,8 +101,25 @@ public class ShoppingManager {
         }
     }
 
-    public void updateItem() {
+    public void updateItem(String oldItem, String newItem) {
+        //varianta 1
+//        deleteItem(oldItem);
+//        addItem(newItem);
 
+        //varianta 2
+        //Metoda Set ne ajuta sa modificam un element de pe o pozitie anume (index)
+        Boolean isPresent = isPresentInList(oldItem);
+        if (isPresent == false) {
+            System.out.println("elementul nu existra ");
+        } else {
+            for (int i = 0; i < itemsList.size(); i++) {
+                String element = itemsList.get(i);
+                if (oldItem.equalsIgnoreCase(element)) {
+                    itemsList.set(i, newItem);
+
+                }
+            }
+        }
     }
 
     public void deleteItem(String item) {
@@ -55,7 +132,7 @@ public class ShoppingManager {
         }
     }
 
-    public boolean isPresentInList (String item){
+    public boolean isPresentInList(String item) {
         boolean isPresent = false;
 
         for (int i = 0; i < itemsList.size(); i++) {
@@ -71,10 +148,13 @@ public class ShoppingManager {
     }
 
     public void showItemsList() {
-        for (int i = 0; i < itemsList.size(); i++) {
-            String element = itemsList.get(i);
-
+        for (String element : itemsList) {
             System.out.println(element);
         }
+        //        for (int i = 0; i < itemsList.size(); i++) {
+        //            String element = itemsList.get(i);
+        //
+        //            System.out.println(element);
+        //        }
     }
 }
